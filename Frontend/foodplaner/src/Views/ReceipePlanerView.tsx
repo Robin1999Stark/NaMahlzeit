a84cba67404c7dcb4626069f86835ce39c856601
Robin1999Stark <r.s.mar@gmx.de>
2023-11-22 22:01:34 +0100

    clean up

--- Frontend/foodplaner/src/Views/ReceipePlanerView.tsx
+++ Frontend/foodplaner/src/Views/ReceipePlanerView.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import MealCard from '../Components/MealCard'
 import { PlanerService } from '../Endpoints/PlanerService'
-import { FoodplanerItem } from '../Datatypes/Meal'
-import AddMealButton from '../Components/AddMealButton'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import { FoodPlaner, FoodplanerItem } from '../Datatypes/Meal'
+import { DragDropContext } from 'react-beautiful-dnd'
+import { mealListID } from '../App'
+import MealList from '../Components/MealList'
+import PlanerList from '../Components/PlanerList'
+import { MealService } from '../Endpoints/MealService'
+import { reorderPlan } from '../reorder'
 
 
 function ReceipePlanerView() {
 
-    const [foodPlaner, setFoodPlaner] = useState<FoodplanerItem[][]>()
-    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const [planer, setPlaner] = useState<FoodPlaner>({})
 
     useEffect(() => {
-        function fillWithEmptyDays(planer: FoodplanerItem[], from: Date, to: Date): FoodplanerItem[] {
+
+        async function fillWithEmptyDays(planer: FoodplanerItem[], from: Date, to: Date): Promise<FoodplanerItem[]> {
             const dates: Date[] = [];
             const currentDate = new Date(from.getTime());
             const newPlaner: FoodplanerItem[] = []
@@ -22,33 +25,48 @@ function ReceipePlanerView() {
                 dates.push(new Date(currentDate.getTime()));
                 currentDate.setDate(currentDate.getDate() + 1);
             }
-            dates.forEach((date, index) => {
-                const item = planer.find((item) => new Date(item.date).getUTCDate() === new Date(date).getUTCDate())
-                if (item === undefined) {
-                    newPlaner.push(new FoodplanerItem(-index, date, []))
-                } else {
-                    newPlaner.push(item)
-                }
-            }
-            )
+
+            // Use Promise.all to wait for all asynchronous calls
+            await Promise.all(
+                dates.map(async (date, index) => {
+                    const item = planer.find((item) => new Date(item.date).getUTCDate() === new Date(date).getUTCDate());
+
+                    if (item === undefined) {
+                        // if a day is missing, create a new Planer for the day in the DB
+                        const newItem = await PlanerService.createPlanerItem({ date, meals: [] });
+                        newPlaner.push(newItem!);
+                    } else {
+                        newPlaner.push(item);
+                    }
+                })
+            );
+
             return newPlaner
         }
 
-        function splitFoodPlaner(planer: FoodplanerItem[], splitSize: number): FoodplanerItem[][] {
-            const batches: FoodplanerItem[][] = []
-            for (let i = 0; i < planer.length; i += splitSize) {
-                batches.push(planer.slice(i, i + splitSize))
-            }
-            return batches;
+        function createFoodPlaner(items: FoodplanerItem[]): FoodPlaner {
+            const foodPlaner: FoodPlaner = {};
+            items.forEach((item) => {
+                foodPlaner[new Date(item.date).toISOString()] = item;
+            })
+            return foodPlaner;
+        }
+
+        async function addMealList(planer: FoodPlaner) {
+            const foodList: number[] = (await MealService.getAllMeals()).map(meal => meal.id);
+            const mealList: FoodplanerItem = new FoodplanerItem(-1, new Date(Date.now()), foodList);
+            planer[mealListID] = mealList;
         }
         async function fetchData() {
             try {
                 const data: FoodplanerItem[] = await PlanerService.getAllPlanerItems();
                 const end = new Date();
                 end.setDate(end.getDate() + 13);
-                const stuffedDate: FoodplanerItem[] = fillWithEmptyDays(data, new Date(Date.now()), end);
-                const splitData: FoodplanerItem[][] = splitFoodPlaner(stuffedDate, 7);
-                setFoodPlaner(splitData)
+                const filledData: FoodplanerItem[] = await fillWithEmptyDays(data, new Date(Date.now()), end);
+                const foodPlaner = createFoodPlaner(filledData);
+                await addMealList(foodPlaner)
+                setPlaner(foodPlaner);
+
             } catch (error) {
                 console.error('Error fetching planer:', error);
             }
@@ -56,85 +74,39 @@ function ReceipePlanerView() {
         fetchData()
     }, [])
     return (
-        <></>
 
-    )
-}
+        <DragDropContext onDragEnd={({ destination, source }) => {
+            if (!destination)
+                return;
+            setPlaner(reorderPlan(planer, source, destination));
+        }}>
+            <div className='flex flex-row justify-between'>
+                <div className='my-6 mx-4 w-[85%] grid grid-flow-row grid-cols-7 gap-3 justify-between'>
+                    {Object.entries(planer).slice(0, -1).map(([key, value]) => (
+                        <PlanerList
+                            internalScroll
+                            key={key}
+                            listId={key}
+                            listType='LIST'
+                            planerItem={value}
+                        />
+                    ))}
+                </div>
+                <div className='w-[15%] my-6 h-full'>
+                    <MealList
+                        internalScroll
+                        key={mealListID}
+                        listId={mealListID}
+                        listType='LIST'
+                    />
 
-{/*<DragDropContext
-            onDragEnd={(result, provided) => {
-                // TODO: implement onDragEnd
-            }}>
-            <div className='flex col-span-3 w-full flex-col items-start max-w-[20rem] flex-wrap'>
-                <h1 className='mb-4 text-2xl font-semibold text-[#181818]'>Foodplaner</h1>
-
-                <table className='table-auto p-3 m-8'>
-                    {foodPlaner ? foodPlaner?.map((planerBatch, idx) => (
-                        <>
-                            <thead>
-                                <tr>
-                                    {planerBatch ? planerBatch?.map((plan, index) => (
-                                        <th className='p-4 justify-start items-center min-w-[8rem]'>
-                                            <h2 className='text text-base font-semibold '>
-                                                {plan.date instanceof Date ? Weekday[plan.date.getDay()] : Weekday[new Date(plan.date).getDay()]}
-                                            </h2>
-                                        </th>
-                                    )) : <></>}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-
-                                    {planerBatch ? planerBatch?.map((plan, index) => (
-                                        <td className='align-top'>
-                                            <Droppable droppableId={idx + "-" + index}>
-
-                                                {(provided) => (
-                                                    <ul
-                                                        ref={provided.innerRef as any}
-                                                        {...provided.droppableProps}
-                                                        key={'list' + idx + "-" + index}
-                                                        className='flex flex-col justify-start'
-                                                    >
-                                                        {plan.food?.map((meal, index) => (
-                                                            <Draggable draggableId={meal + Math.random() + ""} index={index}>
-                                                                {(provided) => (
-                                                                    <li key={index}>
-                                                                        <div
-                                                                            {...provided.draggableProps}
-                                                                            {...provided.dragHandleProps}
-                                                                            ref={provided.innerRef}
-                                                                            className='m-2 px-4 py-1 flex flex-row justify-center items-center rounded-md truncate bg-slate-300'>
-
-                                                                            <MealCard mealID={meal + ""} index={index} />
-                                                                        </div>
-                                                                    </li>)}
-                                                            </Draggable>
-                                                        ))}
-                                                        {<li>
-                                                            <AddMealButton title='Add Meal' />
-                                                        </li>}
-                                                        {provided.placeholder}
-                                                    </ul>
-                                                )}
-                                            </Droppable>
-                                        </td>
-                                    )) : <></>}
-                                </tr>
-
-                            </tbody>
-
-
-                        </>
-                    )) : <></>}
-
-                </table>
-
+                </div>
             </div>
-                                                    </DragDropContext >*/
 
+        </DragDropContext>
+
+    );
 }
-
 
 
 export default ReceipePlanerView
