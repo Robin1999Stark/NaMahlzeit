9c48e690508d6b7863437b5be0e23f142f29d858
Robin1999Stark <r.s.mar@gmx.de>
2023-11-22 22:42:40 +0100

    update

--- Frontend/foodplaner/src/App.tsx
+++ Frontend/foodplaner/src/App.tsx
@@ -1,11 +1,34 @@
+import { Suspense } from "react";
 import ReceipePlanerView from "./Views/ReceipePlanerView";
+import { Route, Routes } from "react-router-dom";
+import CreateMeal from "./Views/CreateMeal";
 
 export const mealListID = "meal-list";
 
 function App() {
   return (
-    <ReceipePlanerView />
+    <>
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Routes>
+          <Route path="/" element={<ReceipePlanerView />} />
+          <Route path="/planer" element={<ReceipePlanerView />} />
+          <Route path="/meals/create" element={<CreateMeal />} />
+
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
 export default App;
+
+/*
+   <Suspense fallback={<div>Loading ...</div>}>
+        <Routes>
+          <Route path="/" element={<ReceipePlanerView />} />
+          <Route path="/planer" element={<ReceipePlanerView />} />
+          <Route path="/meals/create" element={<CreateMeal />} />
+
+        </Routes>
+      </Suspense>
+*/
---
+++ Frontend/foodplaner/src/Views/CreateMeal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateMeal() {
+    return (
+        <div>CreateMeal</div>
+    )
+}
+
+export default CreateMeal
