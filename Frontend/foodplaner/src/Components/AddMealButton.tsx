90ced3d2acab6e86a2db693c7e95a04be07040d5
Robin1999Stark <r.s.mar@gmx.de>
2023-12-08 15:31:37 +0100

    update

--- Frontend/foodplaner/src/Components/AddMealButton.tsx
+++ Frontend/foodplaner/src/Components/AddMealButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function AddMealButton({ title, onClick }: Props) {
     return (
-        <div onClick={onClick} className='m-2 h-full px-4 py-1 flex flex-row justify-center items-center rounded-md truncate border border-slate-600 border-dotted'>
+        <div onClick={onClick} className='m-2 px-4 py-1 flex flex-row justify-center items-center rounded-md truncate border border-slate-00 border-dotted'>
             <h6 className='text-base text-center align-middle truncate text-slate-500 mr-2'>
                 {title}
             </h6>
