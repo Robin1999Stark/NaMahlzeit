b4d01e49eb4a5d33e4dfb1287ab81f73ec838d4c
Robin1999Stark <r.s.mar@gmx.de>
2023-12-11 19:42:40 +0100

    navbar

--- Frontend/foodplaner/src/Components/NavBar.tsx
+++ Frontend/foodplaner/src/Components/NavBar.tsx
@@ -4,7 +4,15 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { MdClose } from "react-icons/md";
 
 function NavBar() {
+
+    const SIZE_MOBILE = 560;
+
     const componentRef = useRef<HTMLDivElement>(null);
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    })
+
     const [clickedOutside, setClickedOutside] = useState(false);
     const [open, setOpen] = useState<boolean>(false);
     const navItems = [
@@ -15,6 +23,14 @@ function NavBar() {
         { link: "/shoppinglist", title: "Shoppinglist" },
 
     ];
+
+    const handleResize = () => {
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+        console.log(windowSize.width)
+    }
 
     const handleClickOutside = (event: MouseEvent) => {
         if (componentRef.current && !componentRef.current.contains(event.target as Node)) {
@@ -27,9 +43,12 @@ function NavBar() {
         // Add a click event listener when the component mounts
         document.addEventListener('click', handleClickOutside);
 
+        window.addEventListener('resize', handleResize);
+
         // Clean up the event listener when the component unmounts
         return () => {
             document.removeEventListener('click', handleClickOutside);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
 
@@ -37,51 +56,75 @@ function NavBar() {
     const handleComponentClick = () => {
         setClickedOutside(false);
     };
+    if (windowSize.width > SIZE_MOBILE) {
+        return (
+            <nav className='flex flex-row h-14 bg-[#181818] z-10 items-center justify-between shadow-md w-full'>
+                <div>
 
-    return (
-        <nav className='flex flex-row h-14 bg-[#181818] z-10 justify-between shadow-md w-full'>
-            {
-                open ?
-                    <button
-                        className='text-3xl text-white p-4'
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            setOpen(!open)
-                        }}>
-                        <MdClose />
+                </div>
 
-                    </button>
-                    :
-                    <button
-                        className='text-3xl text-white p-4'
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            setOpen(!open)
-                        }}>
-                        <RxHamburgerMenu />
-                    </button>
-            }
+                <div className='w-[60%] flex flex-row justify-end items-center mr-8'>
+                    {navItems.map((item) => (
+                        <Link to={item.link}>
+                            <div className='h-full text-lg mx-4 font-normal text-white rounded-md hover:bg-[#fff] hover:text-[#181818]' key={item.title}>
+                                {item.title}
+                            </div>
+                        </Link>
+                    ))
+                    }
 
-            {
-                open ?
-                    <div
-                        ref={componentRef}
-                        onClick={handleComponentClick}
-                        className='bg-white absolute z-10 m-2 top-14 h-fit w-fit shadow-md'>
-                        {
-                            navItems.map((item) => (
-                                <Link to={item.link}>
-                                    <div className='px-6 py-2 m-3 text-lg font-normal rounded-md hover:bg-[#181818] hover:text-white' key={item.title}>
-                                        {item.title}
-                                    </div>
-                                </Link>
-                            ))
-                        }
-                    </div> : <></>
-            }
+                </div>
 
-        </nav>
-    )
+            </nav>
+        )
+    } else {
+
+        return (
+            <nav className='flex flex-row h-14 bg-[#181818] z-10 justify-between shadow-md w-full'>
+                {
+                    open ?
+                        <button
+                            className='text-3xl text-white p-4'
+                            onClick={(event) => {
+                                event.stopPropagation();
+                                setOpen(!open)
+                            }}>
+                            <MdClose />
+
+                        </button>
+                        :
+                        <button
+                            className='text-3xl text-white p-4'
+                            onClick={(event) => {
+                                event.stopPropagation();
+                                setOpen(!open)
+                            }}>
+                            <RxHamburgerMenu />
+                        </button>
+                }
+
+                {
+                    open ?
+                        <div
+                            ref={componentRef}
+                            onClick={handleComponentClick}
+                            className='bg-white absolute z-10 m-2 top-14 h-fit w-fit shadow-md'>
+                            {
+                                navItems.map((item) => (
+                                    <Link to={item.link}>
+                                        <div className='px-6 py-2 m-3 text-lg font-normal rounded-md hover:bg-[#181818] hover:text-white' key={item.title}>
+                                            {item.title}
+                                        </div>
+                                    </Link>
+                                ))
+                            }
+                        </div> : <></>
+                }
+
+            </nav>
+        )
+    }
+
 }
 
 export default NavBar
