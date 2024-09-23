c410560738d5c50706891c8778486ea9a11c8f6a
starkrob <mail@robin-stark.com>
2024-09-23 17:59:19 +0200

    dev envirionment

--- Frontend/frontend/vite.config.ts
+++ Frontend/frontend/vite.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: 3000,
+    port: 3001,
     strictPort: true,
   },
   server: {
-    port: 3000,
+    port: 3001,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3000",
+    origin: "http://0.0.0.0:3001",
   },
 });
