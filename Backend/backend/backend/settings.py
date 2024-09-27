3bc573dd5ad45fc9dbf3203b8360f846b2462118
Robin1999Stark <r.s.mar@gmx.de>
2024-09-27 21:03:30 +0200

    ssl in django

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -139,6 +139,7 @@ USE_I18N = True
 USE_TZ = True
 APPEND_SLASH = False
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = not DEBUG    # Redirect all HTTP requests to HTTPS
 SESSION_COOKIE_SECURE = not DEBUG    # Ensure cookies are only sent over HTTPS
 CSRF_COOKIE_SECURE = not DEBUG    # Ensure CSRF cookies are only sent over HTTPS
